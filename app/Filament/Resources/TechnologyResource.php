22be0ed67ce91e81af985dca53a5587f889be32e
matiz22 <mateuszmalich@gmail.com>
2025-08-09 11:35:44 +0200

    Add Filament resource for Technology management

---
+++ app/Filament/Resources/TechnologyResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TechnologyResource\Pages;
+use App\Models\Technology;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Storage;
+
+
+class TechnologyResource extends Resource
+{
+    protected static ?string $model = Technology::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\Select::make('proficiency_id')
+                    ->relationship('proficiency', 'name'),
+                Forms\Components\FileUpload::make('icon')
+                    ->image()
+                    ->directory('icons')
+                    ->visibility('public')
+                    ->deleteUploadedFileUsing(function ($file) {
+                        if (Storage::exists($file)) {
+                            Storage::delete($file);
+                        }
+                    }),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('proficiency.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTechnologies::route('/'),
+            'create' => Pages\CreateTechnology::route('/create'),
+            'edit' => Pages\EditTechnology::route('/{record}/edit'),
+        ];
+    }
+}
