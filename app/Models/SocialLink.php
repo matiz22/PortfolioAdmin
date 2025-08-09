c534f963124502a8c3e643831ca54bcf67999224
matiz22 <mateuszmalich@gmail.com>
2025-08-09 20:43:54 +0200

    fixing SocialLink.php

--- app/Models/SocialLink.php
+++ app/Models/SocialLink.php
@@ -8,22 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class SocialLink extends Model
 {
     use HasDisplayOrder;
-    /**
-     * The attributes that are mass assignable.
-     */
+
     protected $fillable = [
         'name',
         'url',
-        'icon_class',
+        'icon',
         'order',
-        'is_visible',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     */
     protected $casts = [
-        'is_visible' => 'boolean',
         'order' => 'integer',
     ];
 }
