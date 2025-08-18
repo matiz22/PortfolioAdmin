19dad28e297caf3be9c3cc18643b63157048d5b7
matiz22 <mateuszmalich@gmail.com>
2025-08-18 13:46:31 +0200

    Add OrderedService class for retrieving ordered models with translation support

---
+++ app/Services/OrderedService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use App\Utils\Translatable;
+use Illuminate\Database\Eloquent\Collection;
+
+abstract class OrderedService extends BaseService
+{
+    public function getAllOrderedTranslated(): array
+    {
+        return Translatable::translateCollection($this->getAllOrdered());
+    }
+
+    public function getAllOrdered(): Collection
+    {
+        return $this->getModelClass()::ordered()->get();
+    }
+}
