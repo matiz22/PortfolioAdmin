0cc5e080ab130a6402020b364a30b8d775f0b8d9
matiz22 <mateuszmalich@gmail.com>
2025-09-08 12:51:25 +0200

    fixing relations

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -41,14 +41,21 @@ class Job extends Model
      */
     public function skills(): BelongsToMany
     {
-        return $this->belongsToMany(Skill::class, 'job_skill');
+        return $this->belongsToMany(
+            Skill::class,
+            'job_listing_skill',
+            'job_listing_id',
+            'skill_id'
+        );
     }
 
-    /**
-     * Many-to-many relation to Technologies
-     */
     public function technologies(): BelongsToMany
     {
-        return $this->belongsToMany(Technology::class, 'job_technology');
+        return $this->belongsToMany(
+            Technology::class,
+            'job_listing_technology',
+            'job_listing_id',
+            'technology_id'
+        );
     }
 }
