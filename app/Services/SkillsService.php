0560b04328f7801e12f05611b7c1372502dcefb2
matiz22 <mateuszmalich@gmail.com>
2025-10-27 10:03:16 +0100

    fix wrong relation

--- app/Services/SkillsService.php
+++ app/Services/SkillsService.php
@@ -8,6 +8,6 @@ class SkillsService extends BaseService
 {
     public function __construct()
     {
-        parent::__construct(Skill::class, ['proficiencies']);
+        parent::__construct(Skill::class, ['proficiency']);
     }
 }
