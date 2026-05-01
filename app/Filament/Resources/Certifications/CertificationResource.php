c991b013228aa2f72208586d0355dba4bde4db14
matiz22 <mateuszmalich@gmail.com>
2026-05-01 14:34:30 +0200

    Refactor CertificationResource to enhance layout with organized sections for certification details, credentials, associations, media, and settings

--- app/Filament/Resources/Certifications/CertificationResource.php
+++ app/Filament/Resources/Certifications/CertificationResource.php
@@ -2,18 +2,16 @@
 
 namespace App\Filament\Resources\Certifications;
 
+use App\Filament\Forms\SeoTab;
 use App\Filament\RelationManagers\SkillsRelationManager;
 use App\Filament\RelationManagers\TechnologiesRelationManager;
-use App\Filament\Resources\CertificationResource\Pages;
 use App\Filament\Resources\Certifications\Pages\CreateCertification;
 use App\Filament\Resources\Certifications\Pages\EditCertification;
 use App\Filament\Resources\Certifications\Pages\ListCertifications;
-use App\Filament\Forms\SeoTab;
 use App\Models\Certification;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Schemas\Components\Tabs;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
@@ -22,7 +20,11 @@ use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Resources\Resource;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use LaraZeus\SpatieTranslatable\Resources\Concerns\Translatable;
@@ -41,38 +43,83 @@ class CertificationResource extends Resource
             ->components([
                 Tabs::make('Certification Tabs')
                     ->tabs([
-                        Tabs\Tab::make('Details')
+                        Tabs\Tab::make('Content')
+                            ->icon(Heroicon::DocumentText)
                             ->schema([
-                                Textarea::make('name')
-                                    ->required()
-                                    ->columnSpanFull(),
-                                Textarea::make('short_desc')
-                                    ->columnSpanFull(),
-                                TextInput::make('issuing_organization')
-                                    ->required(),
-                                DatePicker::make('issue_date'),
-                                DatePicker::make('expiration_date'),
-                                TextInput::make('credential_id'),
-                                TextInput::make('credential_url')
-                                    ->url(),
-                                TextInput::make('order')
-                                    ->required()
-                                    ->numeric()
-                                    ->default(0),
-                                Select::make('skills')
-                                    ->label('Skills')
-                                    ->multiple()
-                                    ->relationship('skills', 'name')
-                                    ->searchable(),
-                                Select::make('technologies')
-                                    ->label('Technologies')
-                                    ->multiple()
-                                    ->relationship('technologies', 'name'),
-                                Toggle::make('published'),
-                                Toggle::make('home_page'),
-                                FileUpload::make('thumbnail'),
-                                MarkdownEditor::make('description')
-                                    ->columnSpanFull(),
+                                Section::make('Certification Details')
+                                    ->schema([
+                                        TextInput::make('name')
+                                            ->required(),
+                                        TextInput::make('issuing_organization')
+                                            ->required(),
+                                        Textarea::make('short_desc')
+                                            ->columnSpanFull(),
+                                    ]),
+                                Section::make('Full Description')
+                                    ->schema([
+                                        MarkdownEditor::make('description')
+                                            ->columnSpanFull(),
+                                    ]),
+                            ]),
+                        Tabs\Tab::make('Credentials')
+                            ->icon(Heroicon::Identification)
+                            ->schema([
+                                Section::make('Validation Info')
+                                    ->schema([
+                                        Grid::make(2)->schema([
+                                            TextInput::make('credential_id'),
+                                            TextInput::make('credential_url')
+                                                ->url(),
+                                        ]),
+                                        Grid::make(2)->schema([
+                                            DatePicker::make('issue_date'),
+                                            DatePicker::make('expiration_date'),
+                                        ]),
+                                    ]),
+                            ]),
+                        Tabs\Tab::make('Associations')
+                            ->icon(Heroicon::CommandLine)
+                            ->schema([
+                                Section::make('Skills & Technologies')
+                                    ->schema([
+                                        Grid::make(2)->schema([
+                                            Select::make('skills')
+                                                ->label('Skills')
+                                                ->multiple()
+                                                ->relationship('skills', 'name')
+                                                ->searchable(),
+                                            Select::make('technologies')
+                                                ->label('Technologies')
+                                                ->multiple()
+                                                ->relationship('technologies', 'name'),
+                                        ]),
+                                    ]),
+                            ]),
+                        Tabs\Tab::make('Media')
+                            ->icon(Heroicon::Photo)
+                            ->schema([
+                                Section::make('Media Assets')
+                                    ->schema([
+                                        FileUpload::make('thumbnail')
+                                            ->image()
+                                            ->directory('thumbnails')
+                                            ->visibility('public'),
+                                    ]),
+                            ]),
+                        Tabs\Tab::make('Settings')
+                            ->icon(Heroicon::Cog6Tooth)
+                            ->schema([
+                                Section::make('Visibility & Order')
+                                    ->schema([
+                                        Grid::make(3)->schema([
+                                            Toggle::make('published'),
+                                            Toggle::make('home_page'),
+                                            TextInput::make('order')
+                                                ->required()
+                                                ->numeric()
+                                                ->default(0),
+                                        ]),
+                                    ]),
                             ]),
                         SeoTab::make(),
                     ])
@@ -125,7 +172,7 @@ class CertificationResource extends Resource
     {
         return [
             TechnologiesRelationManager::class,
-            SkillsRelationManager::class
+            SkillsRelationManager::class,
         ];
     }
 
