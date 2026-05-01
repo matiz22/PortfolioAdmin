7c3f839b547451dae8dce421b00b2ea1bba766b6
matiz22 <mateuszmalich@gmail.com>
2026-05-01 14:34:04 +0200

    Refactor SkillResource to organize fields under a Skill Details section and enhance form layout

--- app/Filament/Resources/Realizations/RealizationResource.php
+++ app/Filament/Resources/Realizations/RealizationResource.php
@@ -2,18 +2,16 @@
 
 namespace App\Filament\Resources\Realizations;
 
+use App\Filament\Forms\SeoTab;
 use App\Filament\RelationManagers\SkillsRelationManager;
 use App\Filament\RelationManagers\TechnologiesRelationManager;
-use App\Filament\Resources\RealizationResource\Pages;
 use App\Filament\Resources\Realizations\Pages\CreateRealization;
 use App\Filament\Resources\Realizations\Pages\EditRealization;
 use App\Filament\Resources\Realizations\Pages\ListRealizations;
-use App\Filament\Forms\SeoTab;
 use App\Models\Realization;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Schemas\Components\Tabs;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Select;
@@ -21,7 +19,11 @@ use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Resources\Resource;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use LaraZeus\SpatieTranslatable\Resources\Concerns\Translatable;
@@ -40,53 +42,95 @@ class RealizationResource extends Resource
             ->components([
                 Tabs::make('Realization Tabs')
                     ->tabs([
-                        Tabs\Tab::make('Details')
+                        Tabs\Tab::make('Content')
+                            ->icon(Heroicon::DocumentText)
                             ->schema([
-                                TextInput::make('title')
-                                    ->required(),
-                                TextInput::make('client_name')
-                                    ->required(),
-                                Textarea::make('short_desc')
-                                    ->required()
-                                    ->columnSpanFull(),
-                                FileUpload::make('client_logo')
-                                    ->image()
-                                    ->directory('clients')
-                                    ->visibility('public'),
-                                FileUpload::make('thumbnail')
-                                    ->image()
-                                    ->directory('thumbnails')
-                                    ->visibility('public'),
-                                TextInput::make('client_url')
-                                    ->url(),
-                                TextInput::make('location'),
-                                Select::make('technologies')
-                                    ->relationship('technologies', 'name')
-                                    ->multiple()
-                                    ->preload()
-                                    ->searchable(),
-                                Select::make('skills')
-                                    ->relationship('skills', 'name')
-                                    ->multiple()
-                                    ->preload()
-                                    ->searchable(),
-                                Toggle::make('home_page')
-                                    ->label('Show on Home Page')
-                                    ->default(false),
-                                Toggle::make('published')
-                                    ->default(false),
-                                TextInput::make('order')
-                                    ->required()
-                                    ->numeric()
-                                    ->default(0),
-                                MarkdownEditor::make('description')
-                                    ->columnSpanFull(),
-                                FileUpload::make('screenshots')
-                                    ->multiple()
-                                    ->image()
-                                    ->directory('screenshots')
-                                    ->visibility('public')
-                                    ->columnSpanFull(),
+                                Section::make('Project Information')
+                                    ->schema([
+                                        TextInput::make('title')
+                                            ->required(),
+                                        Textarea::make('short_desc')
+                                            ->required()
+                                            ->columnSpanFull(),
+                                    ]),
+                                Section::make('Detailed Description')
+                                    ->schema([
+                                        MarkdownEditor::make('description')
+                                            ->columnSpanFull(),
+                                    ]),
+                            ]),
+                        Tabs\Tab::make('Client')
+                            ->icon(Heroicon::UserGroup)
+                            ->schema([
+                                Section::make('Client Details')
+                                    ->schema([
+                                        Grid::make(2)->schema([
+                                            TextInput::make('client_name')
+                                                ->required(),
+                                            TextInput::make('client_url')
+                                                ->url(),
+                                        ]),
+                                        TextInput::make('location'),
+                                        FileUpload::make('client_logo')
+                                            ->image()
+                                            ->directory('clients')
+                                            ->visibility('public'),
+                                    ]),
+                            ]),
+                        Tabs\Tab::make('Media')
+                            ->icon(Heroicon::Photo)
+                            ->schema([
+                                Section::make('Visual Assets')
+                                    ->schema([
+                                        Grid::make(2)->schema([
+                                            FileUpload::make('thumbnail')
+                                                ->image()
+                                                ->directory('thumbnails')
+                                                ->visibility('public'),
+                                            FileUpload::make('screenshots')
+                                                ->multiple()
+                                                ->image()
+                                                ->directory('screenshots')
+                                                ->visibility('public'),
+                                        ]),
+                                    ]),
+                            ]),
+                        Tabs\Tab::make('Associations')
+                            ->icon(Heroicon::CommandLine)
+                            ->schema([
+                                Section::make('Skills & Technologies')
+                                    ->schema([
+                                        Grid::make(2)->schema([
+                                            Select::make('skills')
+                                                ->relationship('skills', 'name')
+                                                ->multiple()
+                                                ->preload()
+                                                ->searchable(),
+                                            Select::make('technologies')
+                                                ->relationship('technologies', 'name')
+                                                ->multiple()
+                                                ->preload()
+                                                ->searchable(),
+                                        ]),
+                                    ]),
+                            ]),
+                        Tabs\Tab::make('Settings')
+                            ->icon(Heroicon::Cog6Tooth)
+                            ->schema([
+                                Section::make('Visibility & Order')
+                                    ->schema([
+                                        Grid::make(3)->schema([
+                                            Toggle::make('published')
+                                                ->default(false),
+                                            Toggle::make('home_page')
+                                                ->label('Show on Home Page')
+                                                ->default(false),
+                                            TextInput::make('order')
+                                                ->required()
+                                                ->numeric()
+                                                ->default(0),
+                                        ]),
+                                    ]),
                             ]),
                         SeoTab::make(),
                     ])
